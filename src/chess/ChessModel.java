cd9034e2dbc78c12ec8292712a0744d3d530336a
ooslof <78106315+ooslof@users.noreply.github.com>
2022-02-20 14:48:34 -0500

    test

--- src/chess/ChessModel.java
+++ src/chess/ChessModel.java
@@ -73,14 +73,11 @@ public class ChessModel implements IChessModel {
 
 	/**
 	 * just a test
-<<<<<<< Updated upstream
 	 * test
 	 * test
 	 * test
 	 *
-=======
 	 * Gabe Override
->>>>>>> Stashed changes
 	 */
 	public void AI() {
 		/*
