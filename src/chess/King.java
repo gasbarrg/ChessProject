f2178db7531d66ae1ed8e2fdcc7c45014836782b
Justin <jjburch.tree@gmail.com>
2022-02-27 21:48:36 -0500

    fixed king

--- src/chess/King.java
+++ src/chess/King.java
@@ -6,17 +6,19 @@ public class King extends ChessPiece {
 		super(player);
 	}
 
+	@Override
 	public String type() {
 		return "King";
 	}
 
-
+	@Override
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
 		// More code is needed
 
 		//top 3 squares
 		if ((move.toRow + 1 == move.fromRow && (move.toColumn + 1 == move.fromColumn ||
 				move.toColumn - 1 == move.fromColumn || move.toColumn == move.fromColumn))) {
+			//if space has nothing on it or occupied by other player
 			if ((board[move.toRow][move.toColumn] == null ||
 					board[move.toRow][move.toColumn].player() != this.player())) {
 				return true;
@@ -25,6 +27,7 @@ public class King extends ChessPiece {
 		//middle 3 squares
 		if ((move.toRow == move.fromRow && (move.toColumn + 1 == move.fromColumn ||
 				move.toColumn - 1 == move.fromColumn || move.toColumn == move.fromColumn))) {
+			//if space has nothing on it or occupied by other player
 			if ((board[move.toRow][move.toColumn] == null ||
 					board[move.toRow][move.toColumn].player() != this.player())) {
 				return true;
@@ -33,6 +36,7 @@ public class King extends ChessPiece {
 		//bottom 3 squares
 		if ((move.toRow - 1 == move.fromRow && (move.toColumn + 1 == move.fromColumn ||
 				move.toColumn - 1 == move.fromColumn || move.toColumn == move.fromColumn))) {
+			//if space has nothing on it or occupied by other player
 			if ((board[move.toRow][move.toColumn] == null ||
 					board[move.toRow][move.toColumn].player() != this.player())) {
 				return true;
